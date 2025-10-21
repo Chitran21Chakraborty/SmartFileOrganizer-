e14cc64d010a184ce170870fe7813de8c84f0d6d
Chitran Chakraborty <chitran21c@gmail.com>
2025-10-21 22:20:20 +0530

    Added config.py with file category mappings

---
+++ config.py
@@ -0,0 +1,6 @@
+CATEGORIES = {
+    "Documents": [".pdf", ".docx", ".txt", ".pptx", ".xlsx"],
+    "Images": [".jpg", ".jpeg", ".png", ".gif"],
+    "Videos": [".mp4", ".mkv", ".mov"],
+}
+DEFAULT_CATEGORY = "Others"
